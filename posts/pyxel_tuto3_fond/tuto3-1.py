115449ea9dded698761b5a3bd92d68b0b2f73a6b
Fabrice Lallemand <19834776+fabricelallemand15@users.noreply.github.com>
2023-10-22 20:59:36 +0200

    bis

--- posts/pyxel_tuto3_fond/tuto3-1.py
+++ posts/pyxel_tuto3_fond/tuto3-1.py
@@ -2,7 +2,7 @@ import pyxel
 
 class App:
     def __init__(self):
-        pyxel.init(256, 128, title="Tuto 2", quit_key=pyxel.KEY_Q, fps=30)
+        pyxel.init(256, 128, fps=30)
         # on charge le fichier de ressources
         pyxel.load("jump_game.pyxres")
         # variable définissant l'abscisse initiale du personnage
@@ -29,14 +29,14 @@ class App:
     def draw(self):
         # on efface l'écran et on remplit la fenêtre de bleu
         pyxel.cls(12)
-        # on affiche le personnage à l'abscisse x_personnage et à l'ordonnée 128 - 16
+        # on affiche le personnage à l'abscisse x_personnage et à l'ordonnée 255 - 16
         if self.personnage_marche:
             if pyxel.frame_count % 6 < 3:
-                pyxel.blt(self.x_personnage, pyxel.height - 16, 0, 0, 0, self.direction*16, 16)
+                pyxel.blt(self.x_personnage, pyxel.height-16, 0, 0, 0, self.direction*16, 16)
             else:
-                pyxel.blt(self.x_personnage, pyxel.height - 16, 0, 16, 0, self.direction*16, 16)
+                pyxel.blt(self.x_personnage, pyxel.height-16, 0, 16, 0, self.direction*16, 16)
             self.personnage_marche = False
         else:
-            pyxel.blt(self.x_personnage, pyxel.height - 16, 0, 0, 0, self.direction*16, 16)
+            pyxel.blt(self.x_personnage, pyxel.height-16, 0, 0, 0, self.direction*16, 16)
 
 App()
